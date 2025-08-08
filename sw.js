878d82ac2621ab09a1a97e41ffff8eedece453ee
MidnightDrake <albertom5584@gmail.com>
2025-08-08 10:43:27 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-const CACHE_NAME = 'pf-cache-v1';
+const CACHE_NAME = 'pf-cache-v3';
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./']))); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
