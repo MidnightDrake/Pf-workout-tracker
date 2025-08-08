fd6194b869ceb9b6aa02a194b588aef6f4a06e6f
MidnightDrake <albertom5584@gmail.com>
2025-08-08 09:52:29 -0500

    Create Sw.js

---
+++ Sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'pf-cache-v1';
+const ASSETS = ['./','./index.html','./app.js','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const copy = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, copy)).catch(()=>{});
+      return resp;
+    }).catch(()=>r))
+  );
+});
